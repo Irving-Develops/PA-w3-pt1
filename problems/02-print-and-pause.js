28d744cf2b0a0baa5cf038053dca57a89a22aaa0
Irving Arreola Palacios <irving.a789@gmail.com>
2022-04-01 17:16:18 -0500

    02 edit

--- problems/02-print-and-pause.js
+++ problems/02-print-and-pause.js
@@ -5,7 +5,6 @@ approach to solve this problem.
 
 Example:
 
-printAndPause([200, 800, 200, 800, 200, 800])
 200
 // pause 200ms
 800
@@ -23,10 +22,27 @@ Run tests on just this file by typing `npm test test/02-print-and-pause-spec.js`
 on the command line.
 
 ***********************************************************************/
-
 function printAndPause(nums) {
-  // Your code here
+  if(nums.length === 0) return;
+let currentDelay = nums.shift();
+  setTimeout(
+
+
 }
+
+//console.log(printAndPause())
+// function printAndPause(nums) {
+  
+  
+//   setTimeout(()=>{
+//     let currentDelay = nums.shift();
+//     if(nums.length === 0 ) return;
+//     console.log(currentDelay)
+//   },currentDelay);
+//   return printAndPause(nums);
+  
+// }
+console.log(printAndPause([2000, 8000, 2000, 8000, 2000, 8000]));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
